16a536a3cd4696831df942b89a3e7e3afa884606
km190668496 <mistrykishan186@gmail.com>
2026-01-20 19:38:38 +1300

    Added version number

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+const VERSION = "1.0.0";
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -18,14 +19,19 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/auth" element={<Auth />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
+          <div className="relative min-h-screen">
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/auth" element={<Auth />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+            <div className="fixed bottom-4 right-4 text-xs text-gray-500 pointer-events-none">
+              v{VERSION}
+            </div>
+          </div>
         </TooltipProvider>
       </GameProvider>
     </AuthProvider>
